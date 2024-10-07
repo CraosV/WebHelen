326928df3a7b96d7a2bcae7c23ebefe07a697544
Cristhian Villanueva <villanuevacristhian656@gmail.com>
2024-10-07 18:02:38 -0400

    .

--- view/navbar.php
+++ view/navbar.php
@@ -1,39 +1,47 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a href="index.php"><img src="view/img/logo.jpg" class="img-fluid px-2" ></a>
+        
+        <a href="index.php" class="navbar-brand">
+            <img src="view/img/logo.jpg" class="img-fluid" style="max-width: 150px;" alt="Logo">
+        </a>
+        
+        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
+        
+        
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5"> 
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">INICIO</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page"  data-bs-toggle="modal" data-bs-target="#iniciarSesion">INICIAR SESION</a>
+                    <a class="nav-link" data-bs-toggle="modal" data-bs-target="#iniciarSesion">INICIAR SESIÓN</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="modal" data-bs-target="#crearCuenta">CREAR CUENTA</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    PRODUCTOS
+                        PRODUCTOS
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#">MONITORES</a></li>
                         <li><a class="dropdown-item" href="#">HEADSETS</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">MAS ACCESORIOS</a></li>
+                        <li><a class="dropdown-item" href="#">MÁS ACCESORIOS</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">OFERTAS DE VENTA</a>
                 </li>
             </ul>
+            
             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
                 <button class="btn btn-outline-success" type="submit">Buscar</button>
             </form>
         </div>
     </div>
-  </nav>
+</nav>
